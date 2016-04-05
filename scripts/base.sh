e8154d8a4276afac095859adcb23cad6f08bbf75
Kerem Avcı <keremavci@users.noreply.github.com>
2016-04-05 10:31:16 +0300

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 sudo yum -y update
 sudo yum -y install ansible gcc bzip2 make kernel-headers dkms kernel-devel-$(uname -r)
 
